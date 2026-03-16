1f03483101c30aae8b5b2d25a5d6bb748c449ed7
Dipanshu CHoudhary <Dipanshuchoudhary109@gmail.com>
2026-03-16 20:16:42 +0530

    Enhance quiz detail page interface and question display

--- frontend/src/app/(protected)/quiz/[id]/page.tsx
+++ frontend/src/app/(protected)/quiz/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation"
 import { QuizWorkspaceClient } from "@/features/quiz/QuizWorkspaceClient"
 
 interface Props {
@@ -5,6 +6,10 @@ interface Props {
 }
 
 export default function QuizWorkspacePage({ params }: Props) {
+  const isUuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(params.id)
+  if (!isUuid) {
+    redirect(`/exam/${params.id}/start`)
+  }
   return <QuizWorkspaceClient quizId={params.id} />
 }
 
